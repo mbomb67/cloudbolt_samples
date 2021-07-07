4ed35030384fb3e8635179a6d44282aab02f6c1e
Mike Bombard <37379633+mbomb67@users.noreply.github.com>
2021-07-07 11:49:42 -0400

    Updates

--- blueprints/arm_builder/get_environment_location.py
+++ blueprints/arm_builder/get_environment_location.py
@@ -1,12 +1,8 @@
 """
 This is a helper action for a blueprint constructed by the ARM Builder
-blueprint. This action will return the values available for a parameter on
-a selected environment. For this action to work properly, you will need to
-set up your field as dependent on the parameter you want to pull dropdowns
-from. You will also want to change the FIELD_NAME variable to reflect the
-parameter you want to pull from the environment. The values specified in the
-environment MUST be part of the allowedValues set on the ARM template parameter
-if allowedValues is set on the parameter
+blueprint. This action will return the location that the environment selected
+is tied to. For this action to work properly, you will need to
+set up your field as dependent on the environment parameter.
 """
 from common.methods import set_progress
 from infrastructure.models import Environment
