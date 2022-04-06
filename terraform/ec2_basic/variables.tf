25f9125a3d575ba33b72d0c5a1cd9cfadf15a1c0
Mike Bombard <37379633+mbomb67@users.noreply.github.com>
2022-04-06 14:13:17 -0400

    Update variables.tf

---
+++ terraform/ec2_basic/variables.tf
@@ -0,0 +1,23 @@
+variable "owner_group" {
+  type = map
+}
+
+variable "ami_id" {
+  type = string
+  default = "ami-097e7e9ff9678704a"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "ec2_name" {
+  type = string
+  default = "mb-tst-123"
+}
+
+variable "subnet_id" {
+  type = string
+  default = "subnet-09918fa2d642e5cc3"
+}
